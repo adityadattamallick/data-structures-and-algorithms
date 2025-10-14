bcc5ef02ef886a7ccd48753ae15e404ed8814fab
adityadattamallick <adityadatta30@gmail.com>
2025-10-15 03:07:33 +0600

    Added some information

--- recursion/tree-recursion/tree_recursion.cpp
+++ recursion/tree-recursion/tree_recursion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+// Here function fun() is "Tree Recursive Function" which has more than one or, multiple recursive calls in it. Time Complexity for this is O(2^n), Space Complexity is O(n) - As at most n number of activation records are created at once.
 void fun(int n) {
     if (n > 0) {
         std::cout << n << std::endl;
